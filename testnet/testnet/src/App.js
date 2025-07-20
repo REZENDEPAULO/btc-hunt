8071f4feb68bf4bc98989a26f4915a7a7d8e58e2
REZENDEPAULO <paulo_vudu@hotmail.com>
2025-07-19 23:18:29 -0300

    Create App.js

---
+++ testnet/testnet/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import BTC_Testnet from "./BTC_Testnet";
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: "Arial", padding: 20, maxWidth: 600, margin: "auto" }}>
+      <h1>BTC Hunt - Testnet</h1>
+      <BTC_Testnet />
+    </div>
+  );
+}
