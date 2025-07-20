b10c282462bcfd2688f9ffb7696f5fbfe013e32a
REZENDEPAULO <paulo_vudu@hotmail.com>
2025-07-20 00:02:25 -0300

    Update App.js

--- testnet/testnet/src/App.js
+++ testnet/testnet/src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import BTC_Testnet from "./BTC_Testnet";
+import Home from "./components/Home";
 
 export default function App() {
   return (
     <div style={{ fontFamily: "Arial", padding: 20, maxWidth: 600, margin: "auto" }}>
-      <h1>BTC Hunt - Testnet</h1>
-      <BTC_Testnet />
+      <Home />
     </div>
   );
 }
